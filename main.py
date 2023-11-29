8f5cee40de197f193000d4b6309cb5736b1317b5
jish4593k <143131599+jish4593k@users.noreply.github.com>
2023-11-29 21:06:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import manhattan_distances
 import matplotlib.pyplot as plt
 import seaborn as sns
-import tensorflow as tf
-from tensorflow import keras
-from scipy.cluster.vq import vq
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
 class SpotifyRecommendation:
     def __init__(self, dataset_path):
+        # Load the dataset
         self.spotify_df = pd.read_csv(dataset_path)
         self._prepare_data()
 
@@ -22,56 +20,53 @@ class SpotifyRecommendation:
         # Drop unnecessary columns
         self.spotify_df.drop(columns=['id', 'name', 'release_date', 'year', 'artists'], inplace=True)
 
-      
+        # Visualize correlation matrix using heatmap
         corr_matrix = self.spotify_df.corr()
         sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
         plt.show()
 
-        
+        # Normalize the data using MinMaxScaler
         datatypes = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
         normalization_data = self.spotify_df.select_dtypes(include=datatypes)
         scaler = MinMaxScaler()
         self.normalized_data = pd.DataFrame(scaler.fit_transform(normalization_data), columns=normalization_data.columns)
 
-
+        # Use K-means clustering to assign different clusters
         kmeans = KMeans(n_clusters=10)
         self.spotify_df['features'] = kmeans.fit_predict(self.normalized_data)
 
     def recommend_songs(self, song_name, amount=1):
+       
         song = self.spotify_df[self.spotify_df.name.str.lower() == song_name.lower()].head(1).values[0]
         rec = self.spotify_df[self.spotify_df.name.str.lower() != song_name.lower()]
 
-        
         distances = manhattan_distances(rec.drop(columns=['name', 'artists', 'features']), [song[2:]])
 
         rec['distance'] = distances.flatten()
         rec = rec.sort_values('distance')
 
-  
+       
         columns = ['artists', 'name']
         return rec[columns][:amount]
 
-def load_image():
-    file_path = filedialog.askopenfilename()
-    return Image.open(file_path)
+def load_and_recommend():
+    
+    window = tk.Tk()
+    window.title("Spotify Song Recommendation")
+    window.geometry("800x600")
 
-def display_image(image):
-    img = ImageTk.PhotoImage(image)
-    panel = tk.Label(window, image=img)
-    panel.image = img
-    panel.pack()
+  
+    dataset_path = filedialog.askopenfilename()
 
-window = tk.Tk()
-window.title("Spotify Song Recommendation System")
-window.geometry("800x600")
+    recommendation_model = SpotifyRecommendation(dataset_path)
 
 
-load_data_button = tk.Button(window, text="Load Spotify Dataset", command=lambda: recommendation_model._prepare_data())
-load_data_button.pack()
+    song_name = input("Enter the song name for recommendations: ")
 
 
-recommend_button = tk.Button(window, text="Recommend Songs", command=lambda: display_image(recommendation_model.recommend_songs("Mixe", 10)))
-recommend_button.pack()
+    recommendations = recommendation_model.recommend_songs(song_name, 10)
+    print(recommendations)
 
+    window.mainloop()
 
-window.mainloop()
+load_and_recommend()
